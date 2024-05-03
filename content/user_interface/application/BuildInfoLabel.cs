c8bc5da1eb778ccec166025dc82f956e8a7ba240
ucanluc <umut.can.uluc@gmail.com>
2024-05-03 22:08:51 +0300

    Create BuildInfoLabel.cs

---
+++ content/user_interface/application/BuildInfoLabel.cs
@@ -0,0 +1,21 @@
+﻿using Godot;
+
+namespace Solace.content.user_interface.application;
+
+public partial class BuildInfoLabel : Label
+{
+    private const string VersionNamePath = "application/config/version";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _EnterTree()
+    {
+        UpdateLabel();
+    }
+
+
+    private void UpdateLabel()
+    {
+        var version = ProjectSettings.GetSetting(VersionNamePath).ToString();
+        Text = $"Build {version}";
+    }
+}
