17f74d0bd2ab3cd2a580cade3003fdd59d5fa4d4
ucanluc <umut.can.uluc@gmail.com>
2024-05-05 22:39:26 +0300

    2d navigation example created

---
+++ apps/visibility_2d/TopdownNPC.cs
@@ -0,0 +1,61 @@
+using Godot;
+
+namespace Solace.apps.visibility_2d;
+
+public partial class TopdownNPC : CharacterBody2D
+{
+    [Export] private Node2D targetObject;
+
+    private NavigationAgent2D _navigationAgent;
+
+    [Export]
+    private float _movementSpeed = 200.0f;
+
+    public Vector2 MovementTarget
+    {
+        get { return _navigationAgent.TargetPosition; }
+        set { _navigationAgent.TargetPosition = value; }
+    }
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        _navigationAgent = GetNode<NavigationAgent2D>("NavigationAgent2D");
+
+        // These values need to be adjusted for the actor's speed
+        // and the navigation layout.
+        _navigationAgent.PathDesiredDistance = 4.0f;
+        _navigationAgent.TargetDesiredDistance = 4.0f;
+
+        // Make sure to not await during _Ready.
+        Callable.From(ActorSetup).CallDeferred();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+        if (_navigationAgent.IsNavigationFinished())
+        {
+            MovementTarget = targetObject.GlobalPosition;
+            return;
+        }
+
+        Vector2 currentAgentPosition = GlobalTransform.Origin;
+        Vector2 nextPathPosition = _navigationAgent.GetNextPathPosition();
+
+        Velocity = currentAgentPosition.DirectionTo(nextPathPosition) * _movementSpeed;
+        MoveAndSlide();
+    }
+
+    private async void ActorSetup()
+    {
+        // Wait for the first physics frame so the NavigationServer can sync.
+        await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
+
+        // Now that the navigation map is no longer empty, set the movement target.
+        MovementTarget = targetObject.GlobalPosition;
+        ;
+    }
+}
