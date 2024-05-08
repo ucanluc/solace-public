99841f85f4174e77f8b53eb210ba33c7c1169956
ucanluc <umut.can.uluc@gmail.com>
2024-05-08 12:55:22 +0300

    added first unit test; and Rider support

---
+++ main/test/ExampleTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Examples;
+
+using GdUnit4;
+using static GdUnit4.Assertions;
+
+[TestSuite]
+public class ExampleTest
+{
+    [TestCase]
+    public void ExampleTestSuccessWhenRan()
+    {
+        AssertBool(true).IsTrue();
+    }
+
+
+}
